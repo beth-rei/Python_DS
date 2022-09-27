57586cef8e439969ca1ba105df8f88958694b4aa
BETH REICHERTER <bethreicherter@gmail.com>
2022-09-27 15:29:46 -0400

    python_ds

---
+++ 26_vowel_count/vowel_count.py
@@ -0,0 +1,23 @@
+VOWELS = set('aeiou')
+
+
+def vowel_count(phrase):
+    """Return frequency map of vowels, case-insensitive.
+
+        >>> vowel_count('rithm school')
+        {'i': 1, 'o': 2}
+        
+        >>> vowel_count('HOW ARE YOU? i am great!') 
+        {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
+    """
+
+    vowel = ['a', 'e', 'i', 'o', 'u']
+
+    phrase = phrase.lower()
+    counter = {}
+
+    for ltr in phrase:
+        if ltr in VOWELS:
+            counter[ltr] = counter.get(lrt,0) + 1
+
+    return counter
